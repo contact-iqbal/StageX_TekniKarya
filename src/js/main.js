7c23fc26236f38920f0f915d13d6a83bf11202d8
XyStar123123 <hermawanbintang20@gmail.com>
2025-12-21 21:44:30 +0700

    Resources 100%, Service 100% (Cannot implement border blur with svg)

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 import { initLucide } from "./lucide.js";
 import { initDark } from "./dark.js";
 import { initAccordion } from "./accordion.js";
+import { initSlider } from "./slider.js";
 
 initLucide()
 initDark()
 initAccordion()
+initSlider()
---
+++ src/js/slider.js
@@ -0,0 +1,53 @@
+export function initSlider(){
+    const data = [
+        { label: 'Future', img: '/src/img/building.jpg', header: 'Exploring the Path to Better Health', content: 'A trusted health destination, this post uncovers the principles and practices that make our clinic a center for better care.' },
+        { label: 'Techology', img: '/src/img/lab.jpg', header: 'Creating safe medicine for all kind', content: 'A labeled lab that is trusted in each county, preserve 100% safety around all country, serving medicine fast, and high quality.' }
+    ]
+
+    const slider = document.getElementById("slider")
+    const img = slider.querySelector(".slider-image")
+    const label = slider.querySelector("#slider-wrapper .slider-label")
+    const header = slider.querySelector("#slider-wrapper .slider-header")
+    const content = slider.querySelector("#slider-wrapper .slider-content")
+    const dots = document.querySelector(".slider-dot-container")
+    data.forEach((_)=> {
+        const dot = document.createElement("div")
+        dot.className = 'h-2 w-2 rounded-full border border-white slider-dot'
+        dots.appendChild(dot)
+    })
+    const dot = document.querySelectorAll(".slider-dot")
+    dot[0].classList.add("bg-white")
+
+    const end = data.length - 1
+    let tracker = 0
+
+    if(!slider && !img && !label && !header && !content && !dots) return
+
+    function sliderUpdater(i){
+        img.style.backgroundImage = `url(${data[i].img})`
+        label.textContent = data[i].label
+        header.textContent = data[i].header
+        content.textContent = data[i].content
+    }
+
+    function dotUpdater(i){
+        dot.forEach((d, index) => {
+            if(i == index){
+                d.classList.add("bg-white")
+            }else{
+                d.classList.remove("bg-white")
+            }
+        })
+    }
+
+    setInterval(()=>{
+        tracker++
+        
+        if(tracker > end){
+            tracker = 0
+        }
+
+        sliderUpdater(tracker)
+        dotUpdater(tracker)
+    }, 3000)
+}
